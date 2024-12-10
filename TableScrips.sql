4c5c9899d38e433f5b1171f1f725bbe7ac169f1b
Sandhya Tenugu <sandhya.tenugu@snaddevelopers.com>
2024-12-10 12:15:05 +0530

    commiting code and example

---
+++ TableScrips.sql
@@ -0,0 +1,11 @@
+/*--------------------------------------------------------------------------------------------------------
+-------- Table name  : ACR_GL_IMP_FIELD_NAMES_STG
+-------- Description : This table stores the source field names of the Gl import conversion.
+-------- Created by  : Sandhya
+---------------------------------------------------------------------------------------------------------------*/
+
+CREATE TABLE ACR.ACR_GL_IMP_FIELD_NAMES_STG 
+   (FIELD_NAME VARCHAR2(100), 
+	JOB_ID VARCHAR2(100));
+/
+	
